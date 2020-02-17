abb3769e13f00d63e09352cfead977b5572e2ae8
hyogeon <men1219@hanmail.net>
2020-02-17 16:34:41 +0900

    set view property and event by kotlin code

--- app/src/main/java/com/example/hgexam1/MainActivity.kt
+++ app/src/main/java/com/example/hgexam1/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.hgexam1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                if(nameEdit.text.isBlank()){
+                    greetText.text = "이름이 없습니다."
+                }
+                else {
+                    greetText.text = "${nameEdit.text}님, 반갑습니다."
+                }
+            }
+
+        })
     }
 }
