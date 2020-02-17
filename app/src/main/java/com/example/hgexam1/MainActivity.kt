da7c301f1cd6f3934a5eb8db6acb58508bfaff8e
hyogeon <men1219@hanmail.net>
2020-02-17 17:15:33 +0900

    ImageView

--- app/src/main/java/com/example/hgexam1/MainActivity.kt
+++ app/src/main/java/com/example/hgexam1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hgexam1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,16 +12,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        inputButton.setOnClickListener(object: View.OnClickListener{
+        inputButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                if(nameEdit.text.isBlank()){
+                if (nameEdit.text.isBlank()) {
                     greetText.text = "이름이 없습니다."
-                }
-                else {
+                } else {
                     greetText.text = "${nameEdit.text}님, 반갑습니다."
                 }
             }
+        })
 
+        photoView.setOnClickListener(object : View.OnClickListener {
+            var isDefault = true
+            override fun onClick(v: View?) {
+                isDefault = !isDefault
+                if (isDefault) photoView.setImageResource(R.drawable.iu1)
+                else photoView.setImageResource(R.drawable.iu2)
+            }
+
+        })
+
+        scaleTypeButton.setOnClickListener(object: View.OnClickListener {
+            var isDefault = true
+            override fun onClick(v: View?) {
+                isDefault = !isDefault
+                if(isDefault) photoView.scaleType = ImageView.ScaleType.CENTER_CROP
+                else photoView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            }
         })
     }
 }
